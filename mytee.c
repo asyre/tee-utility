9d0479d960931d2f22d48e34d9d25f59719a242f
asyre <katderbi@gmail.com>
2019-11-01 21:45:08 +0300

    new version

--- mytee.c
+++ mytee.c
@@ -1,11 +1,8 @@
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <stdbool.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
-#include <sys/ioctl.h>
 #include <getopt.h>
 
 #define MAX 8192
@@ -20,12 +17,7 @@ char *read_from_stdin(int *read_count);
 
 mode_t command_flag_parser(char **argv, int argc, int *filePosition);
 
-bool is_stdin_empty(void);
-
 int command_processor(int argc, char **argv);
-
-
-void sighandler(int signal);
 
 int main(int argc, char **argv) {
     return command_processor(argc, argv);
@@ -42,7 +34,8 @@ int *open_file(char **argv, mode_t mode, int argc, int file_position, int *files
         } else if (strcmp(argv[i], ">>") == 0) {
             mode = O_WRONLY | O_APPEND;
             i++;
-        } else if ((strcmp(argv[i], "--") == 0)) i++;
+        }
+//        } else if ((strcmp(argv[i], "--") == 0)) i++;
         fileDescriptor = open(argv[i], mode | O_CREAT, 0644);
         if (fileDescriptor < 0)
             print_error();
@@ -53,35 +46,24 @@ int *open_file(char **argv, mode_t mode, int argc, int file_position, int *files
 }
 
 int command_processor(int argc, char **argv) {
-//    int count = 0;
-//    int ptrSize = 1;
+    int count = 0;
     char c[1];
     c[0] = (char) 0;
-//    char *ptr = malloc(sizeof(char));
     int filePosition = 1;
     mode_t mode = command_flag_parser(argv, argc, &filePosition);
     int files_count = 0;
     int *files = open_file(argv, mode, argc, filePosition, &files_count);
-    return 0;
-}
-
-
-char *read_line(int *read_count) {
-    *read_count = 0;
-    int sum_read = 0;
-    int count;
-    char *lines = malloc(sizeof(char));
-    int i = 1;
     do {
-        count = read(STDIN_FILENO, lines + i - 1, sizeof(char));
-        if (count > 0) {
-            sum_read += count;
-            lines = realloc(lines, sizeof(char) * ++i + 1);
+        char *buf = read_from_stdin(&count);
+        for (int i = 0; i < files_count; ++i) {
+            write_to(files[i], buf, count);
         }
-    } while (lines[i - 2] != '\n');
-    (*read_count) = sum_read;
-    write_to(STDOUT_FILENO, lines, *read_count);
-    return lines;
+        free(buf);
+    } while (count != 0);
+    for (int i = 0; i < files_count; ++i) {
+        close_file(files[i]);
+    }
+    return 0;
 }
 
 char *read_from_stdin(int *read_count) {
@@ -93,64 +75,57 @@ char *read_from_stdin(int *read_count) {
 }
 
 
-//int write_in_file(char *buf, int count) {
-//    write_to(fileDescriptor, buf, count);
-//    close_file(fileDescriptor);
-//    return 0;
-//}
-
 mode_t command_flag_parser(char **argv, int argc, int *filePosition) {
     *filePosition = 1;
     mode_t mode = O_WRONLY | O_TRUNC;
-
-    const char *short_options = "ai";
-
-    const struct option long_options[] = {
-            {"help", no_argument, NULL, 'h'}
-    };
     int val;
-    opterr = 0;
-    while ((val = getopt_long(argc, argv, short_options,
-                              long_options, NULL)) != -1) {
+//    opterr = 0;
+    while ((val = getopt(argc, argv, "ai")) != -1) {
         switch (val) {
-            case 'h': {
-                if ((strcmp(argv[1], "--help") == 0)) {
-                    write_to(STDERR_FILENO, "usage: tee [-ai] [file ...]\n", 29);
-                    exit(0);
-                }
-                break;
-            }
-            case 'a': {
+            case 'a':
                 ++*filePosition;
                 mode = O_WRONLY | O_APPEND;
                 break;
-            }
-
-            case 'i': {
+            case 'i':
                 (void) signal(SIGINT, SIG_IGN);
                 ++*filePosition;
                 break;
-            }
-            case '?':
             default:
-                break;
+                write_to(STDERR_FILENO, "usage: tee [-ai] [file ...]\n", 29);
+                exit(0);
         }
     }
+
+//    const char *short_options = "ai";
+//
+//    const struct option long_options[] = {
+//            {"help", no_argument, NULL, 'h'}
+//    };
+//    while ((val = getopt_long(argc, argv, short_options,
+//                              long_options, NULL)) != -1) {
+//        switch (val) {
+//            case 'a': {
+//                ++*filePosition;
+//                mode = O_WRONLY | O_APPEND;
+//                break;
+//            }
+//
+//            case 'i': {
+//                (void) signal(SIGINT, SIG_IGN);
+//                ++*filePosition;
+//                break;
+//            }
+//            case '?':
+//            default:
+//            case 'h': {
+//                write_to(STDERR_FILENO, "usage: tee [-ai] [file ...]\n", 29);
+//                exit(0);
+//            }
+//        }
+//    }
     return mode;
 }
 
-void sighandler() {
-}
-
-bool is_stdin_empty(void) {
-    fd_set rd;
-    struct timeval tv = {0};
-    int ret;
-    FD_ZERO(&rd);
-    FD_SET(STDIN_FILENO, &rd);
-    ret = select(1, &rd, NULL, NULL, &tv);
-    return (ret == 0);
-}
 
 void write_to(int file_desc, char *buf, long count) {
     ssize_t bytes_read = 0;
